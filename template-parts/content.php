cba74a362db898c2949dd9766d2159f152fb0cd5
José Marques <josemarques@gmail.com>
2017-01-10 23:23:58 +0000

    Updates post markup

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,18 +12,27 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+		$post_thumbnail_ID = get_post_thumbnail_id($post->ID);
+		if ( $post_thumbnail_ID != "" ){
+			$img_src = wp_get_attachment_image_url( $post_thumbnail_ID, 'medium' );
+			$img_srcset = wp_get_attachment_image_srcset( $post_thumbnail_ID, 'medium' ); ?>
+			<img src="<?php echo esc_url( $img_src ); ?>"
+					 srcset="<?php echo esc_attr( $img_srcset ); ?>" />
+			<?php } ?>
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php simpla_dois_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		<div class="entry-header__wrapper">
+			<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
+
+			if ( 'post' === get_post_type() ) : ?>
+
+			<?php
+			endif; ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -42,6 +51,9 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		<div class="entry-meta">
+			<?php simpla_dois_posted_on(); ?>
+		</div><!-- .entry-meta -->
 		<?php simpla_dois_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
